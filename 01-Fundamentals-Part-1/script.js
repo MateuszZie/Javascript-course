88aad2c0d412d558edb730fc0a0d5da520d61981
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-08 14:03:26 +0100

    Values and Veriables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,8 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is Fun!');
-20 + 15 + 15;
 console.log(20 + 15 + 15);
+let firstName = "Mateusz";
+let PI = 3.1415;
+let firstNumber = 120.35;
+console.log(firstName);
+console.log(PI);
+console.log(firstNumber);
