5d8b0f1e345909de360f62431761891d0c226973
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-10 20:54:48 +0100

    Data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,8 +1,27 @@
+// Start
+/*
 let js = 'amazing';
 console.log(20 + 15 + 15);
 let firstName = "Mateusz";
-let PI = 3.1415;
+let PI = 
 let firstNumber = 120.35;
 console.log(firstName);
 console.log(PI);
 console.log(firstNumber);
+*/
+// Data tapes
+let javaScriptIsFun = true;
+console.log(true);
+console.log(javaScriptIsFun);
+console.log(typeof true);
+console.log(typeof 'Jonas');
+console.log(typeof 34);
+javaScriptIsFun = 'Mateusz';
+console.log(typeof javaScriptIsFun);
+let noName;
+console.log(noName);
+console.log(typeof noName);
+let PI = 3.1415;
+console.log(typeof PI);
+console.log(null);
+console.log(typeof null);
