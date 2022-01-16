24a3f58c506a5f507e3fa43bda863022e6775492
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-16 09:37:10 +0100

    Active Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Active Strict Mode
+let hasDriversLicens = false;
+const passTest = true;
+
+if (passTest) hasDriverLicens = true;
+if (hasDriversLicens) console.log("Test Passed");
+
+const interface = 23445;
