965b3b550d48e92f3ecde4cd72d0fb81a12cf142
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-16 09:59:27 +0100

    Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Active Strict Mode
+/*
 let hasDriversLicens = false;
 const passTest = true;
 
@@ -8,3 +9,29 @@ if (passTest) hasDriverLicens = true;
 if (hasDriversLicens) console.log("Test Passed");
 
 const interface = 23445;
+*/
+// Fuctions
+
+function logger() {
+    console.log("My name is Mateusz");
+}
+
+logger();
+logger();
+logger();
+
+function juiceProcesor(apples, oranges) {
+    console.log(apples, oranges);
+    return `Juice from ${apples} Apples and ${oranges} Oranges`;
+}
+
+const appleJuice = juiceProcesor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = juiceProcesor(3, 4);
+console.log(appleOrangeJuice);
+
+console.log(juiceProcesor(6, 4));
+
+const num = Number('25');
+console.log(num);
