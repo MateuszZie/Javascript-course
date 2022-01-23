e6c79d944a786d6ca9cfcb48c654b0ab59b709da
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-23 13:33:27 +0100

    Setting up Prettier and VS Code

---
+++ 03-Developer-Skills/script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = 23;
+const mateusz = "Mateusz lol'mnn";
+
+const age = birthYear => 2022 - birthYear;
+console.log(age(1986));
