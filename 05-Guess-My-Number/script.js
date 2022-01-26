0650c11eade4d0eec9797324bed997dafac49d61
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-26 17:50:25 +0100

    Handling Click Events

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,8 +1,21 @@
 'use strict';
 
+/*
+// Selecting and Manipulating Elements
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number!';
 document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+// Handling Click Events
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Chose only number\nbetween 1 and 20';
+  }
+});
