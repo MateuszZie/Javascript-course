04bf9c5f70795810e3c9c8624d74127e1a7ee16a
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-26 18:28:09 +0100

    Implementing the Game Logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -10,12 +10,33 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 // Handling Click Events
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+let gameOver = false;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent =
-      'Chose only number\nbetween 1 and 20';
+  if (!gameOver) {
+    if (!guess) {
+      document.querySelector('.message').textContent =
+        'Chose only number\nbetween 1 and 20';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Game Over You Won!!!';
+      gameOver = true;
+    } else if (guess > secretNumber) {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too High!';
+    } else {
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.message').textContent = 'Too Low!';
+    }
+    if (score === 0) {
+      document.querySelector('.message').textContent = 'Game Over You Lose!!!';
+      gameOver = true;
+    }
   }
 });
