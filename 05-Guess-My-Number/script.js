268cde50b7cb08b985107b2af42b7a720eaab71b
Mateusz Ziebura <mateusz_860@op.pl>
2022-01-29 09:17:24 +0100

    Implementing Highscores

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 // Handling Click Events
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 let gameOver = false;
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +28,10 @@ document.querySelector('.check').addEventListener('click', () => {
       document.querySelector('body').style.backgroundColor = '#60b347';
       document.querySelector('.number').style.width = '30rem';
       document.querySelector('.number').textContent = secretNumber;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
     } else if (guess > secretNumber) {
       score--;
       document.querySelector('.score').textContent = score;
