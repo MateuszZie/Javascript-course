818599d8e118f17d38753d5d5cbad03735de6d53
Mateusz Ziebura <mateusz_860@op.pl>
2022-02-05 18:53:04 +0100

    Enhanced Object Literals

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -5,18 +5,35 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`sat`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  hours: openingHours,
 
-  order: function (startIndex, mainIndex) {
+  order(startIndex, mainIndex) {
     return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
   },
 
-  deliver: function ({
+  deliver({
     adress = 'Rybnik',
     time = '22:00',
     indexMainMenu = 0,
@@ -27,38 +44,27 @@ const restaurant = {
     );
   },
 
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(`Here you have your past with ${ing1}, ${ing2} and ${ing3}`);
   },
 
-  orderPiza: function (mainIngridient, ...restIngridiens) {
+  orderPiza(mainIngridient, ...restIngridiens) {
     console.log(mainIngridient);
     console.log(restIngridiens);
   },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
 
+console.log(restaurant);
+
+/*
+// Looping Arrays: The for-of Loop
 const main = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for (const item of main) console.log(item);
 
 for (const [i, elm] of main.entries()) console.log(`${i + 1}: ${elm}`);
 
-/*
+
 // Logical Assignment Operators
 const rest1 = {
   name: 'Pizza Hut',
