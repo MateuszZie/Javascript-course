bcddf1710471a9537fb01a42fd1336972274eca1
Mateusz Ziebura <mateusz_860@op.pl>
2022-02-19 15:30:07 +0100

    How Passing Arguments Works: Value vs. Reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+const flightNum = 'L123';
+const passanger = {
+  name: 'Mateusz',
+  pesel: 435435435435,
+};
+
+const bookingSystem = function (flightNum, passanger) {
+  flightNum = 'K432';
+  passanger.name = 'Mr. ' + passanger.name;
+
+  if (passanger.pesel === 435435435435) {
+    alert('Check in');
+  } else {
+    alert('Incorrect pesel');
+  }
+};
+
+const newPesel = function (passanger) {
+  passanger.pesel = Math.trunc(Math.random() * 1000000000);
+};
+
+console.log(flightNum, passanger);
+newPesel(passanger);
+bookingSystem(flightNum, passanger);
+console.log(flightNum, passanger);
+
+/*
+// Default Parameters
 const bookings = [];
 
 const createBoking = function (
@@ -19,3 +47,4 @@ const createBoking = function (
 createBoking('L15', undefined, 15);
 createBoking('L15', 10, 15);
 createBoking('L15', 10);
+*/
