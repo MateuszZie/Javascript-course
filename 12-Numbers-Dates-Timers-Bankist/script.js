e4d3b4cc984241b29600a7d16700186a0900b149
Mateusz Ziebura <mateusz_860@op.pl>
2022-03-06 13:23:23 +0100

    Math and Rounding

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,6 +252,40 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+
+console.log(Math.max(2, 45, 67, 456, 55));
+console.log(Math.max(2, 45, '67', 456, 55));
+console.log(Math.max(2, 45, '67px', 456, 55));
+
+console.log(Math.min(2, 45, 67, 456, 55));
+
+console.log(Math.floor(Math.random() * 5 + 1));
+
+const randomBetween = (min, max) =>
+  console.log(Math.floor(Math.random() * (max - min) + 1 + min));
+randomBetween(10, 20);
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+console.log('=========');
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+console.log('=========');
+console.log(Math.floor(23.3));
+console.log(Math.floor('23.9'));
+console.log('=========');
+console.log(Math.floor(-23.3));
+console.log(Math.trunc(-23.3));
+
+console.log((23).toFixed(2));
+console.log((23.43545).toFixed(2));
+console.log(+(23.45).toFixed(5));
+
+/*
+// Converting and Checking Numbers
 console.log(23 === 23.0);
 console.log(Number('23'));
 console.log(0.1 + 0.2);
@@ -280,3 +314,4 @@ console.log(Number.isInteger(20.5));
 console.log(Number.isInteger('20'));
 console.log(Number.isInteger(Number('20x')));
 console.log(Number.isInteger(20 / 0));
+*/
