1c13dbefc849504d1ecdebde34085ddc26fb5583
Mateusz Ziebura <mateusz_860@op.pl>
2022-03-17 17:56:37 +0100

    Prototypes

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -7,3 +7,15 @@ const Person = function (firstName, birthDay) {
 const mateusz = new Person('Mateusz', 1986);
 console.log(mateusz instanceof Person);
 console.log(mateusz);
+
+Person.prototype.calcAge = function () {
+  console.log(new Date().getFullYear() - this.birthDay);
+};
+
+console.log(mateusz.calcAge());
+console.log(mateusz.__proto__ === Person.prototype);
+Person.prototype.species = 'Homo Sapiens';
+
+console.log(Person.prototype.isPrototypeOf(mateusz));
+console.log(mateusz.hasOwnProperty('firstName'));
+console.log(mateusz.hasOwnProperty('species'));
