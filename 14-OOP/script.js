b8435ac77ca16cece9f4e01cab763ce080ce2220
Mateusz Ziebura <mateusz_860@op.pl>
2022-03-17 18:27:03 +0100

    Prototypal Inheritance on Built-In Objects

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -19,3 +19,21 @@ Person.prototype.species = 'Homo Sapiens';
 console.log(Person.prototype.isPrototypeOf(mateusz));
 console.log(mateusz.hasOwnProperty('firstName'));
 console.log(mateusz.hasOwnProperty('species'));
+
+console.log(mateusz.__proto__);
+console.log(mateusz.__proto__.__proto__);
+console.log(mateusz.__proto__.__proto__.__proto__);
+
+const arr = [1, 2, 3, 4, 2, 3, 3, 4, 3, 2, 3, 2, 3, 3];
+
+console.log(arr.__proto__);
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__.__proto__);
+
+console.log(arr.__proto__ === Array.prototype);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
