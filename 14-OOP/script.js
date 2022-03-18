37a3f63291984dd511eff8c756950e5cc893a69c
Mateusz Ziebura <mateusz_860@op.pl>
2022-03-18 20:52:59 +0100

    Setters and Getters

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -41,22 +41,56 @@ console.log(arr.unique());
 */
 
 class PersonCl {
-  constructor(firstName, birthYear) {
+  constructor(fullName, birthYear) {
     this.birthYear = birthYear;
-    this.firstName = firstName;
+    this.fullName = fullName;
   }
 
   calcAge() {
     console.log(new Date().getFullYear() - this.birthYear);
   }
+
+  get age() {
+    return new Date().getFullYear() - this.birthYear;
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not full Name`);
+  }
 }
 
-const darek = new PersonCl('Darek', 1973);
+const darek = new PersonCl('Darek Z', 1973);
+
+console.log(darek);
+
+console.log(darek.age);
+
+console.log(darek.fullName);
 
 PersonCl.prototype.greet = function () {
-  console.log(`Hey ${this.firstName}`);
+  console.log(`Hey ${this.fullName}`);
 };
 
 darek.greet();
 darek.calcAge();
 console.log(darek);
+
+const account = {
+  owner: 'Mateusz',
+  movments: [100, 300, 200, 500],
+
+  get latest() {
+    return this.movments.slice(-1).pop();
+  },
+  set latest(num) {
+    this.movments.push(num);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.latest);
