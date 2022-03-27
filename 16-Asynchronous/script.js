67c1319a007a9fb01c08da83b7a196adf7947265
Mateusz Ziebura <mateusz_860@op.pl>
2022-03-27 19:16:25 +0200

    Our First AJAX Call: XMLHttpRequest

---
+++ 16-Asynchronous/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const getCountry = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `<article class="country">
+  <img class="country__img" src="${data[0].flags.png}" />
+  <div class="country__data">
+    <h3 class="country__name">${data[0].name.common}</h3>
+    <h4 class="country__region">${data[0].continents[0]}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data[0].population / 1000000
+    ).toFixed(1)} people</p>
+    <p class="country__row"><span>🗣️</span>${
+      Object.values(data[0].languages)[0]
+    }</p>
+    <p class="country__row"><span>💰</span>${
+      Object.values(data[0].currencies)[0].name
+    }</p>
+  </div>
+</article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountry('poland');
+getCountry('russia');
