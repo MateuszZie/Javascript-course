f2ff0f915875c514a101efb6833ff77e64a097b1
Mateusz Ziebura <mateusz_860@op.pl>
2022-04-12 20:07:29 +0200

    Implementing Pagination - Part 2

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import RecipeView from './view/recipeView.js';
 import SearchView from './view/searchView.js';
 import ResultView from './view/resultView.js';
+import PaginationView from './view/paginationView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime/runtime';
@@ -32,14 +33,20 @@ const controlSerachRecipe = async function () {
     if (!query) return;
     await model.loadSearchRecipe(query);
     ResultView.render(model.getSearchResaultPage());
+    PaginationView.render(model.state.search);
   } catch {
     ResultView.renderError();
   }
 };
 
+const controlPagination = async function (pageNumber) {
+  ResultView.render(model.getSearchResaultPage(pageNumber));
+  PaginationView.render(model.state.search);
+};
 const init = function () {
   RecipeView.addHandlerRender(controlRecipe);
   SearchView.addHandlerSearch(controlSerachRecipe);
+  PaginationView.addHandlerPagination(controlPagination);
 };
 
 init();
---
+++ 18-forkify/src/js/view/paginationView.js
@@ -0,0 +1,66 @@
+import View from './view';
+import icon from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+  _errorMeassage = `Error in pagination view`;
+  _meesage = '';
+
+  addHandlerPagination(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      e.preventDefault();
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      handler(+btn.dataset.page);
+    });
+  }
+
+  _markupData() {
+    const curPage = this._data.page;
+    const maxPage = Math.ceil(
+      this._data.recipes.length / this._data.resultPerPage
+    );
+    console.log(this._data.recipes.length);
+    if (curPage === 1 && maxPage > 1) {
+      return `<button data-page="${
+        curPage + 1
+      }" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+        <use href="${icon}.svg#icon-arrow-right"></use>
+      </svg>
+    </button>`;
+    }
+    if (curPage === maxPage && maxPage > 1) {
+      return `<button data-page="${
+        curPage - 1
+      }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icon}.svg#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage - 1}</span>
+    </button>`;
+    }
+    if (maxPage > 1) {
+      return `<button data-page="${
+        curPage - 1
+      }" class="btn--inline pagination__btn--prev">
+      <svg class="search__icon">
+        <use href="${icon}.svg#icon-arrow-left"></use>
+      </svg>
+      <span>Page ${curPage - 1}</span>
+    </button>
+    <button data-page="${
+      curPage + 1
+    }" class="btn--inline pagination__btn--next">
+      <span>Page ${curPage + 1}</span>
+      <svg class="search__icon">
+        <use href="${icon}.svg#icon-arrow-right"></use>
+      </svg>
+    </button>`;
+    }
+    return '';
+  }
+}
+
+export default new PaginationView();
